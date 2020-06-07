5f02870b65c89877c4d78f03044f3c5facd0040c
hotdari90 <hotdari90@snbplus.co.kr>
2020-06-07 12:54:21 +0900

    make navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
