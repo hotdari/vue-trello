6c586d02c7657b2bca3febd3bc301f2ea1113b06
hotdari <38778044+hotdari@users.noreply.github.com>
2020-06-13 13:06:11 +0900

    login-fetch

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthrorized = () => {
-  router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -15,13 +15,23 @@ const request = (method, url, data) => {
   }).then(result => result.data)
     .catch(result => {
       const { status } = result.response
-      if (status === UNAUTHORIZED) return onUnauthrorized()
-      throw Error(result)
+      if (status === UNAUTHORIZED) onUnauthrorized()
+      throw result.response
     })
+}
+
+export const setAuythInHeader = token => {
+  axios.defaults.headers.common.Authorization = token ? `Bearer ${token}` : null
 }
 
 export const board = {
   fetch () {
-    return request('get', 'boards')
+    return request('get', '/boards')
+  }
+}
+
+export const auth = {
+  login (email, password) {
+    return request('post', '/login', { email, password })
   }
 }
